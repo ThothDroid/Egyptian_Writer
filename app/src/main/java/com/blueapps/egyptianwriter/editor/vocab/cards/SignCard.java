5b21c3d17fd51e61203edb6a21cb673f07dd3ed4
cristmasbox <sonne.zucker@gmx.de>
2026-04-09 18:07:42 +0200

    added SignCard

---
+++ app/src/main/java/com/blueapps/egyptianwriter/editor/vocab/cards/SignCard.java
@@ -0,0 +1,38 @@
+package com.blueapps.egyptianwriter.editor.vocab.cards;
+
+import static com.blueapps.egyptianwriter.editor.vocab.FileMaster.XML_TAG_SIGN;
+
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+
+import com.blueapps.signprovider.SignProvider;
+
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.Text;
+import org.xmlpull.v1.XmlPullParserException;
+
+import java.io.IOException;
+
+public class SignCard extends Card {
+
+    private String signId = null;
+
+    public SignCard(Element element, int index) {
+        super(element, index);
+
+        Node signNode = element.getElementsByTagName(XML_TAG_SIGN).item(0);
+        signId = getChildString(signNode);
+    }
+
+    public String getSignId() {
+        return signId;
+    }
+
+    public Drawable getSign(Context context) throws XmlPullParserException, IOException {
+        SignProvider signProvider = new SignProvider(context);
+
+        return signProvider.getSign(signId);
+    }
+}
