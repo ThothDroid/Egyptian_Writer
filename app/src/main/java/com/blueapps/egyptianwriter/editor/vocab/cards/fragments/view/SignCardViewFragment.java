71932a5a5a161451ae7a26b3b0a2170af0268dd5
cristmasbox <sonne.zucker@gmx.de>
2026-04-10 09:55:37 +0200

    new imports

--- app/src/main/java/com/blueapps/egyptianwriter/editor/vocab/cards/fragments/view/SignCardViewFragment.java
+++ app/src/main/java/com/blueapps/egyptianwriter/editor/vocab/cards/fragments/view/SignCardViewFragment.java
@@ -6,6 +6,8 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.os.Parcelable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
