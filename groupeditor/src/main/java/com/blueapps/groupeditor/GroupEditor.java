94d027f211c4e9c763110f657cac9e527bb8ca55
ThothDroid <website-tutorials@gmx.de>
2026-08-18 20:34:32 +0200

    add: initialize SignProvider in GroupEditor and implement drawing logic

--- groupeditor/src/main/java/com/blueapps/groupeditor/GroupEditor.java
+++ groupeditor/src/main/java/com/blueapps/groupeditor/GroupEditor.java
@@ -2,31 +2,48 @@ package com.blueapps.groupeditor;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
 import android.view.View;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
+import com.blueapps.signprovider.SignProvider;
+
+import org.xmlpull.v1.XmlPullParserException;
+
+import java.io.IOException;
+
 public class GroupEditor extends View {
+
+    SignProvider signProvider;
 
     // Values
     private String signId = "";
 
     public GroupEditor(Context context) {
         super(context);
+        constructor();
     }
 
     public GroupEditor(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
+        constructor();
     }
 
     public GroupEditor(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        constructor();
     }
 
     public GroupEditor(Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
         super(context, attrs, defStyleAttr, defStyleRes);
+        constructor();
+    }
+
+    private void constructor(){
+        signProvider = new SignProvider(getContext());
     }
 
 
@@ -34,6 +51,13 @@ public class GroupEditor extends View {
     protected void onDraw(@NonNull Canvas canvas) {
         super.onDraw(canvas);
 
+        // get Sign
+        try {
+            Drawable drawable = signProvider.getSign(signId);
+            drawable.draw(canvas);
+        } catch (IOException | XmlPullParserException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 
@@ -44,5 +68,6 @@ public class GroupEditor extends View {
 
     public void setSignId(String signId) {
         this.signId = signId;
+        this.invalidate();
     }
 }
