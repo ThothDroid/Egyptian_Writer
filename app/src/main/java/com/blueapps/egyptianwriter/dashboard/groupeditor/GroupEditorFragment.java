0e8381e7fccad49e9199e081f8292a1f36471a12
ThothDroid <website-tutorials@gmx.de>
2026-08-20 19:12:52 +0200

    implement listener

--- app/src/main/java/com/blueapps/egyptianwriter/dashboard/groupeditor/GroupEditorFragment.java
+++ app/src/main/java/com/blueapps/egyptianwriter/dashboard/groupeditor/GroupEditorFragment.java
@@ -7,25 +7,29 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.blueapps.egyptianwriter.databinding.FragmentGroupEditorBinding;
+import com.blueapps.groupeditor.Group;
 import com.blueapps.groupeditor.GroupEditor;
+import com.blueapps.groupeditor.GroupListener;
 
 import org.xmlpull.v1.XmlPullParserException;
 
 import java.io.IOException;
 
-public class GroupEditorFragment extends Fragment {
+public class GroupEditorFragment extends Fragment implements GroupListener {
 
     private FragmentGroupEditorBinding binding;
 
     // Views
     private GroupEditor groupEditor;
     private EditText inputId;
+    private TextView idText;
 
     @Nullable
     @Override
@@ -38,6 +42,7 @@ public class GroupEditorFragment extends Fragment {
         // initialize views
         groupEditor = binding.groupEditor;
         inputId = binding.inputId;
+        idText = binding.textId;
 
         inputId.addTextChangedListener(new TextWatcher() {
             @Override
@@ -60,7 +65,14 @@ public class GroupEditorFragment extends Fragment {
             }
         });
 
+        groupEditor.addGroupListener(this);
+
         return rootView;
 
+    }
+
+    @Override
+    public void onGroupChanged(Group group) {
+        idText.setText(group.getSignId());
     }
 }
