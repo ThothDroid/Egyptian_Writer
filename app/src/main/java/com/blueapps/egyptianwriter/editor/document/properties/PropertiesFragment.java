63c4877f27cae5891f782ef30167037eb92ade5e
ThothDroid <website-tutorials@gmx.de>
2026-08-22 18:52:30 +0200

    enable emergency saving

--- app/src/main/java/com/blueapps/egyptianwriter/editor/document/properties/PropertiesFragment.java
+++ app/src/main/java/com/blueapps/egyptianwriter/editor/document/properties/PropertiesFragment.java
@@ -121,6 +121,19 @@ public class PropertiesFragment extends Fragment implements TextWatcher {
     @Override
     public void onDestroyView() {
         super.onDestroyView();
+        // Emergency saving
+        try {
+            int textSize = Integer.parseInt(editTextSize.getText().toString());
+            if (textSize > 0 && textSize < 1000) {
+                propertiesManager.setTextSize((int) TypedValueCompat.spToPx(textSize, getResources().getDisplayMetrics()));
+                rightFormat();
+            } else {
+                wrongFormat(String.format(ERROR_WRONG_RANGE, 0, 1000));
+            }
+        } catch (NumberFormatException e) {
+            e.printStackTrace();
+            wrongFormat(ERROR_WRONG_FORMAT);
+        }
         binding = null;
     }
 
