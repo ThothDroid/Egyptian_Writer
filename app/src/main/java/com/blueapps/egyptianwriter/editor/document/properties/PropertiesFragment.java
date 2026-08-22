806e8c3928ae9dc92d450962f63e40d193135946
ThothDroid <website-tutorials@gmx.de>
2026-08-22 18:40:56 +0200

    convert px to sp

--- app/src/main/java/com/blueapps/egyptianwriter/editor/document/properties/PropertiesFragment.java
+++ app/src/main/java/com/blueapps/egyptianwriter/editor/document/properties/PropertiesFragment.java
@@ -53,7 +53,7 @@ public class PropertiesFragment extends Fragment implements TextWatcher {
         textSizeDecrease = binding.textSizeDecrease;
 
         // Set initial values
-        editTextSize.setText(propertiesManager.getTextSize().getValue().toString());
+        editTextSize.setText((int) TypedValueCompat.pxToSp(propertiesManager.getTextSize().getValue(), getResources().getDisplayMetrics()));
 
         // textSize
         Handler handler = new Handler(Looper.getMainLooper());
