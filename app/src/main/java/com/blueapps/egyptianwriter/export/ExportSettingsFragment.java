6abd7fb0fc32943beb49cc95c207257b36f69391
ThothDroid <website-tutorials@gmx.de>
2026-09-12 14:33:49 +0200

    add file format selection and update learn more text based on selected format

--- app/src/main/java/com/blueapps/egyptianwriter/export/ExportSettingsFragment.java
+++ app/src/main/java/com/blueapps/egyptianwriter/export/ExportSettingsFragment.java
@@ -9,6 +9,8 @@ import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.blueapps.egyptianwriter.R;
@@ -20,12 +22,19 @@ public class ExportSettingsFragment extends Fragment {
 
     private FragmentExportSettingsBinding binding;
 
+    private int fileType = FILE_TYPE_EWDOC;
+
     // Views
     private TextView learnMoreTitle;
     private ExpandableLayout learnMoreLayout;
     private TextView learnMoreText;
+    private Spinner fileFormat;
 
     // Constants
+    public static final int FILE_TYPE_EWDOC = 0;
+    public static final int FILE_TYPE_SVG = 1;
+    public static final int FILE_TYPE_PNG = 2;
+    public static final int FILE_TYPE_JPEG = 3;
     /*private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";*/
 
@@ -62,6 +71,7 @@ public class ExportSettingsFragment extends Fragment {
         binding = FragmentExportSettingsBinding.inflate(inflater);
 
         // init Views
+        fileFormat = binding.fileFormat;
         learnMoreTitle = binding.learnMoreTitle;
         learnMoreLayout = binding.learnMoreLayout;
         learnMoreText = binding.learnMoreText;
@@ -77,7 +87,28 @@ public class ExportSettingsFragment extends Fragment {
         });
         learnMoreLayout.collapse(false);
         learnMoreTitle.setBackground(ContextCompat.getDrawable(getContext(), R.drawable.clickable_textview_bg));
-        learnMoreText.setText(Html.fromHtml(ContextCompat.getString(getContext(), R.string.export_learn_more_svg), Html.FROM_HTML_MODE_COMPACT));
+        learnMoreText.setText(Html.fromHtml(ContextCompat.getString(getContext(), R.string.export_learn_more_ewdoc), Html.FROM_HTML_MODE_COMPACT));
+
+        fileFormat.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                fileType = i;
+                if (fileType == FILE_TYPE_EWDOC){
+                    learnMoreText.setText(Html.fromHtml(ContextCompat.getString(getContext(), R.string.export_learn_more_ewdoc), Html.FROM_HTML_MODE_COMPACT));
+                } else if (fileType == FILE_TYPE_SVG){
+                    learnMoreText.setText(Html.fromHtml(ContextCompat.getString(getContext(), R.string.export_learn_more_svg), Html.FROM_HTML_MODE_COMPACT));
+                } else if (fileType == FILE_TYPE_PNG){
+                    learnMoreText.setText(Html.fromHtml(ContextCompat.getString(getContext(), R.string.export_learn_more_png), Html.FROM_HTML_MODE_COMPACT));
+                } else if (fileType == FILE_TYPE_JPEG){
+                    learnMoreText.setText(Html.fromHtml(ContextCompat.getString(getContext(), R.string.export_learn_more_jpeg), Html.FROM_HTML_MODE_COMPACT));
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
 
         return binding.getRoot();
     }
