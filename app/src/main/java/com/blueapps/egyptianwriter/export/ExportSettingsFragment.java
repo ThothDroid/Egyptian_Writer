52afce0b16aa1939054d40d0ffa67f82d8e702b1
ThothDroid <website-tutorials@gmx.de>
2026-09-12 15:59:57 +0200

    better support for other sdks

--- app/src/main/java/com/blueapps/egyptianwriter/export/ExportSettingsFragment.java
+++ app/src/main/java/com/blueapps/egyptianwriter/export/ExportSettingsFragment.java
@@ -85,16 +85,16 @@ public class ExportSettingsFragment extends Fragment {
                 String html = "";
                 if (fileType == FILE_TYPE_EWDOC){
                     noSettings.setVisibility(View.VISIBLE);
-                    html = ContextCompat.getString(getContext(), R.string.export_learn_more_ewdoc)
+                    html = ContextCompat.getString(getContext(), R.string.export_learn_more_ewdoc);
                 } else if (fileType == FILE_TYPE_SVG){
                     noSettings.setVisibility(View.GONE);
-                    html = ContextCompat.getString(getContext(), R.string.export_learn_more_svg)
+                    html = ContextCompat.getString(getContext(), R.string.export_learn_more_svg);
                 } else if (fileType == FILE_TYPE_PNG){
                     noSettings.setVisibility(View.GONE);
-                    html = ContextCompat.getString(getContext(), R.string.export_learn_more_png)
+                    html = ContextCompat.getString(getContext(), R.string.export_learn_more_png);
                 } else if (fileType == FILE_TYPE_JPEG){
                     noSettings.setVisibility(View.GONE);
-                    html = ContextCompat.getString(getContext(), R.string.export_learn_more_jpeg)
+                    html = ContextCompat.getString(getContext(), R.string.export_learn_more_jpeg);
                 }
                 if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
                     learnMoreText.setText(Html.fromHtml(html, Html.FROM_HTML_MODE_LEGACY));
