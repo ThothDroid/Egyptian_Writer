71d23e6f6162f03b3ac8ddb0c8cb9f105cf46391
ThothDroid <website-tutorials@gmx.de>
2026-09-12 11:40:59 +0200

    add expandable learn more section in export settings

--- app/src/main/java/com/blueapps/egyptianwriter/export/ExportSettingsFragment.java
+++ app/src/main/java/com/blueapps/egyptianwriter/export/ExportSettingsFragment.java
@@ -2,18 +2,26 @@ package com.blueapps.egyptianwriter.export;
 
 import android.os.Bundle;
 
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.blueapps.egyptianwriter.R;
 import com.blueapps.egyptianwriter.databinding.FragmentExportSettingsBinding;
 
+import net.cachapa.expandablelayout.ExpandableLayout;
+
 public class ExportSettingsFragment extends Fragment {
 
     private FragmentExportSettingsBinding binding;
+
+    // Views
+    private TextView learnMoreTitle;
+    private ExpandableLayout learnMoreLayout;
 
     // Constants
     /*private static final String ARG_PARAM1 = "param1";
@@ -51,7 +59,21 @@ public class ExportSettingsFragment extends Fragment {
         // Inflate the layout for this fragment
         binding = FragmentExportSettingsBinding.inflate(inflater);
 
-        //
+        // init Views
+        learnMoreTitle = binding.learnMoreTitle;
+        learnMoreLayout = binding.learnMoreLayout;
+
+        learnMoreTitle.setOnClickListener((view) -> {
+            if (learnMoreLayout.isExpanded()){
+                learnMoreTitle.setBackground(ContextCompat.getDrawable(getContext(), R.drawable.clickable_textview_bg));
+                learnMoreLayout.collapse(true);
+            } else {
+                learnMoreTitle.setBackground(ContextCompat.getDrawable(getContext(), R.drawable.context_textview_bg));
+                learnMoreLayout.expand(true);
+            }
+        });
+        learnMoreLayout.collapse(false);
+        learnMoreTitle.setBackground(ContextCompat.getDrawable(getContext(), R.drawable.clickable_textview_bg));
 
         return binding.getRoot();
     }
